de5ff495fcd1a231ba0ed0ffd369de22e2628234
ChrisMiraval <62941076+ChrisMiraval@users.noreply.github.com>
2020-04-01 00:50:50 -0500

    Update ruth
    Update the previous ruth

--- DesarrolloWeb/Boletines/javascript/main.js
+++ DesarrolloWeb/Boletines/javascript/main.js
@@ -36,7 +36,7 @@ links.forEach(function(v){
         //Se ejecuta la totalidad del evento despúes de un tiempo X
         setTimeout(function(){
             //Nos direccionamos a la ruta especificada
-            location.href = "/Boletines"
+            location.href = "/DesarrolloWeb/Boletines"
         }, 600);        
 
         return false;
